c30213624f34fc9b3b89ac3d5680dbb1f349ce65
Anirudh Madhusudan <anirudhmadhusudan854@gmail.com>
2021-05-16 16:21:24 +0530

    updated readme

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='src',
+    version='0.0.1',
+    description="its a mlops wineq project",
+    author="Anirudh",
+    package=find_packages(),
+    license="MIT"
+
+
+)
